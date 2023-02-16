9e4acd7b794d3a520c4c11545b39644072cd88c5
Mielesgames <emieltlam11@gmail.com>
2023-02-16 14:21:49 +0100

    Het werkt nu (yay)

---
+++ Days/script.js
@@ -0,0 +1,35 @@
+// '|' means workday, '||' means weekend
+var Days = ["|Monday", "|Tuesday", "|Wednesday", "|Thursday", "|Friday", "||Saturday", "||Sunday","||Mario Dag"];
+var InversedDays = [...Days].reverse();
+
+for (i = 0; i < Days.length; i++){
+    document.getElementById("AllDays").innerText = document.getElementById("AllDays").innerText + Days[i].replace("|", " ").replace("|", " ")
+}
+var amountOfWorkDays = 0; // I want to make the days editable \\
+for (i = 0; i < Days.length; i++){
+    if (!Days[i].includes("||")){
+        document.getElementById("WorkDays").innerText = document.getElementById("WorkDays").innerText + Days[i].replace("|", " ").replace("|", " ")
+    }
+}
+var amountOfWeekendDays = 0;
+for (i = 0; i < Days.length; i++){
+    if (Days[i].includes("||")){
+        document.getElementById("WeekendDays").innerText = document.getElementById("WeekendDays").innerText + Days[i].replace("|", " ").replace("|", " ")
+    }
+}
+
+for (i = 0; i < InversedDays.length; i++){
+    document.getElementById("AllDaysReversed").innerText = document.getElementById("AllDaysReversed").innerText + InversedDays[i].replace("|", " ").replace("|", " ")
+}
+var amountOfWorkDays = 0; // I want to make the days editable \\
+for (i = 0; i < InversedDays.length; i++){
+    if (!InversedDays[i].includes("||")){
+        document.getElementById("WorkDaysReversed").innerText = document.getElementById("WorkDaysReversed").innerText + InversedDays[i].replace("|", " ").replace("|", " ")
+    }
+}
+var amountOfWeekendDays = 0;
+for (i = 0; i < InversedDays.length; i++){
+    if (InversedDays[i].includes("||")){
+        document.getElementById("WeekendDaysReversed").innerText = document.getElementById("WeekendDaysReversed").innerText + InversedDays[i].replace("|", " ").replace("|", " ")
+    }
+}
