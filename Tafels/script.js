3dde052e83eeb49e81862aad8ed0371d57ec6761
Mielesgames <emieltlam11@gmail.com>
2023-02-20 11:41:52 +0100

    complete thing

---
+++ Tafels/script.js
@@ -0,0 +1,12 @@
+var numbers = [1, 2, 3, 4, 5, 23082345678998765432389123]
+var MainDiv = document.getElementById("Main")
+for (i = 0; i < numbers.length; i++){
+    var newDiv = document.createElement("div")
+    for (c = 0; c < 10; c++){
+        var newH1 = document.createElement("h1")
+        newH1.innerText = numbers[i] + "x" + (c+1) + "=" + (numbers[i] * (c+ 1))
+        newDiv.appendChild(newH1)
+    }
+    newDiv.classList.add("MainDivs")
+    MainDiv.appendChild(newDiv)
+}
